b838fa54778dc6919313aa8a43e0a0a1e097e0b3
guillheu <guillheu@gmail.com>
2022-05-01 06:55:58 +0200

    removed unused helper

--- tailor-server-redis/src/dynamic_metadata_fields.rs
+++ tailor-server-redis/src/dynamic_metadata_fields.rs
@@ -5,11 +5,6 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 
 
-pub fn get_helper_helper (h: &Helper, _: &Handlebars, _: &Context, _rc: &mut RenderContext, out: &mut dyn Output) -> HelperResult {
-    out.write(&format!("{:?}", h))?;
-    Ok(())
-}
-
 pub fn static_string_helper (_h: &Helper, _: &Handlebars, _: &Context, _rc: &mut RenderContext, out: &mut dyn Output) -> HelperResult {
     let output_string = "foo bar";
     println!("{}", output_string);
