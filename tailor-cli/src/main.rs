299874c8a94d1be200869f5bc12ba1277429a5bb
guillheu <guillheu@gmail.com>
2022-05-05 10:03:12 +0200

    adding start and stop commands

--- tailor-cli/src/main.rs
+++ tailor-cli/src/main.rs
@@ -13,26 +13,29 @@ use std::collections::HashMap;
 
 static TAILOR_SERVER_DOCKER_REPO: &'static str = "guillh/tailor-server";
 static TAILOR_SERVER_DEFAULT_DOCKER_TAG: &'static str = "0.1.1";
+static CONTAINER_NAME: &'static str = "tailor-server_managed";
 
 
 #[derive(Debug, Subcommand)]
 enum Commands{
     #[clap(arg_required_else_help = true)]
-    /// Initialize a new distributable server directory.
-    /// To then run the server, simply run the tailor-server-redis executable.
-    /// On UNIX systems, add executable permissions to tailor-server-redis
+    /// Initialize a new project.
     Init{
         // Name of the new folder to be created
         folder_name: String,
     },
-    /// Generate a new server directory from a pre-existing example
-    /// To then run the server, simply run the tailor-server-redis executable.
-    /// On UNIX systems, add executable permissions to tailor-server-redis
+    /// Generate a new project from a pre-existing example
     Example{
         /// Name of the example
         example_name: String,
     },
-    // Start,
+    /// Start the server.
+    Start{
+        folder_name: Option<String>,
+        port: Option<u16>,
+    },
+    /// Stop the currently running server
+    Stop,
     /// Publish a server
     /// NOT YET IMPLEMENTED
     /// Eventually will allow to publish metadata and NFTs to either Aleph, IPFS or Arweave
@@ -54,9 +57,11 @@ fn main() {
 
     let args = Cli::parse();
     let result = match args.command {
-        Commands::Init{folder_name: n}      => (init(&n)),
-        Commands::Example{example_name: n}   => example(&n),
-        Commands::Publish   => publish(),
+        Commands::Init{folder_name}         => init(&folder_name),
+        Commands::Example{example_name}     => example(&example_name),
+        Commands::Start{folder_name, port}  => start(folder_name, port),
+        Commands::Stop                      => stop(),
+        Commands::Publish                   => publish(),
     };
     if result.is_err() {
         println!("Error: {}", result.err().unwrap().as_ref());
@@ -65,6 +70,8 @@ fn main() {
         println!("Success!");
     }
 }
+
+//  Commands functions
 
 fn init(name: &str) -> Result<(), Box<dyn std::error::Error>> {
     build_project_from_example("default", name)?;
@@ -76,9 +83,77 @@ fn example(name: &str)  -> Result<(), Box<dyn std::error::Error>>{
     Ok(())
 }
 
+fn start(given_path: Option<String>, given_port: Option<u16>)  -> Result<(), Box<dyn std::error::Error>>{
+    let path = match given_path {
+        Some(path)  => path,
+        None        => {
+            println!("Defaulting to current directory");
+            ".".to_string()
+        },
+    };
+    let port = match given_port {
+        Some(port)  => port,
+        None        => {
+            println!("defaulting to port 8080");
+            8080
+        },
+    };
+
+    let mounts = vec![
+        Mount{
+            Target: "/home/tailor/tailor-server/serve/static".to_string(),
+            Source: format!("{}{}{}{}", current_dir()?.to_str().unwrap(), "/", path, "/static"),
+            Type: "bind".to_string(),
+            ReadOnly: true,
+            Consistency: None,
+            BindOptions: None,
+            VolumeOptions: None,
+            TmpfsOptions: None,
+        },
+        Mount{
+            Target: "/home/tailor/tailor-server/serve/templates".to_string(),
+            Source: format!("{}{}{}{}", current_dir()?.to_str().unwrap(), "/", path, "/templates"),
+            Type: "bind".to_string(),
+            ReadOnly: true,
+            Consistency: None,
+            BindOptions: None,
+            VolumeOptions: None,
+            TmpfsOptions: None,
+        }
+    ];
+
+    let port_binding = PortBinding {
+        HostIp: None,
+        HostPort: port.to_string(),
+    };
+    
+    run_container(Some(port_binding), Some(mounts), None)?;
+    Ok(())
+}
+
+fn stop() -> Result<(), Box<dyn std::error::Error>> {
+    stop_container()?;
+    Ok(())
+}
+
 fn publish() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Other functions
+
+
 
 fn build_project_from_example(example_name: &str, target_folder: &str) -> Result<(), Box<dyn std::error::Error>> {
     println!("Creating {} directory...", target_folder);
@@ -104,7 +179,6 @@ fn build_project_from_example(example_name: &str, target_folder: &str) -> Result
 
 
 fn run_container(port_binding: Option<PortBinding>, mounts: Option<Vec<Mount>>, entrypoint_override: Option<Vec<String>>) -> Result<(), Box<dyn std::error::Error>> {
-    let container_name = String::from("tailor-server_managed");
     println!("Connecting to docker daemon...");
     let mut docker = Docker::connect("unix:///var/run/docker.sock")?;
     println!("Fetching image {}:{}", TAILOR_SERVER_DOCKER_REPO, TAILOR_SERVER_DEFAULT_DOCKER_TAG);
@@ -135,9 +209,17 @@ fn run_container(port_binding: Option<PortBinding>, mounts: Option<Vec<Mount>>, 
         HostConfig: Some(host_config_create),
         Entrypoint: entrypoint_override,
     };
-    println!("Creating ephemeral container {}", &container_name);
-    docker.create_container(container_name.clone(), container_create)?;
-    println!("Starting container {}", &container_name);
-    docker.start_container(&container_name)?;
+    println!("Creating ephemeral container {}", &CONTAINER_NAME);
+    docker.create_container(CONTAINER_NAME.to_string(), container_create)?;
+    println!("Starting container {}", &CONTAINER_NAME);
+    docker.start_container(&CONTAINER_NAME)?;
+    Ok(())
+}
+
+fn stop_container() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Connecting to docker daemon...");
+    let mut docker = Docker::connect("unix:///var/run/docker.sock")?;
+    println!("Stopping container {}...", CONTAINER_NAME);
+    docker.stop_container(&CONTAINER_NAME)?;
     Ok(())
 }
